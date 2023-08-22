bdc5322b12f177cd1a7422e1a946e2206526b475
Jan Carlo Paredes <paredesjancarlo99@gmail.com>
2023-08-23 00:55:49 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main {
+    public static void main(String[] args) {
+        // strings
+        String aString = "Hello World!"; // output "Hello World!"
+
+        // integer types
+        byte aSingleByte = 100; // -128 to 127
+        short aSmallNumber = 20000; // -32,768 to 32,767
+        int anInteger = 2147483647; // -2,147,483,648 to 2,147,483,647
+        long aLargeNumber = 9223372036854775807L; // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        // decimal types
+        double aDouble = 1.7976931348623157E308; // 4.9e-324 to 1.7976931348623157e+308
+        float aFloat = 3.4028235E38f; // 1.4e-045 to 3.4028235e+038
+
+        // booleans
+        boolean aBoolean1 = true; // true or false
+        boolean aBoolean2 = false; // true or false
+
+        // characters
+        char copyrightSymbol = '\u00A9'; // output "©"
+        char registeredSymbol = '\u00AE'; // output "®"
+        char omegaSymbol = '\u03A9'; // output "Ω"
+        char piSymbol = '\u03C0'; // output "π"
+        char sigmaSymbol = '\u03A3'; // output "Σ"
+        char thetaSymbol = '\u03B8'; // output "θ"
+        char phiSymbol = '\u03C6'; // output "φ"
+        char deltaSymbol = '\u03B4'; // output "δ"
+        char lambdaSymbol = '\u03BB'; // output "λ"
+        char gammaSymbol = '\u03B3'; // output "γ"
+        char muSymbol = '\u03BC'; // output "μ"
+        char tauSymbol = '\u03C4'; // output "τ"
+        char epsilonSymbol = '\u03B5'; // output "ε"
+        char rhoSymbol = '\u03C1'; // output "ρ"
+        char etaSymbol = '\u03B7'; // output "η"
+        char xiSymbol = '\u03BE'; // output "ξ"
+        char zetaSymbol = '\u03B6'; // output "ζ"
+        char kappaSymbol = '\u03BA'; // output "κ"
+        char upsilonSymbol = '\u03C5'; // output "υ"
+        char psiSymbol = '\u03C8'; // output "ψ"
+        char chiSymbol = '\u03C7'; // output "χ"
+        char betaSymbol = '\u03B2'; // output "β"
+        char alphaSymbol = '\u03B1'; // output "α"
+        char deltaSymbol2 = '\u0394'; // output "Δ"
+        char lambdaSymbol2 = '\u039B'; // output "Λ"
+        char piSymbol2 = '\u03A0'; // output "Π"
+        char sigmaSymbol2 = '\u03A3'; // output "Σ"
+        char thetaSymbol2 = '\u0398'; // output "Θ"
+        char phiSymbol2 = '\u03A6'; // output "Φ"
+        char omegaSymbol2 = '\u03A9'; // output "Ω"
+        char epsilonSymbol2 = '\u0395'; // output "Ε"
+        char rhoSymbol2 = '\u03A1'; // output "Ρ"
+        char etaSymbol2 = '\u0397'; // output "Η"
+        char xiSymbol2 = '\u039E'; // output "Ξ"
+        char zetaSymbol2 = '\u0396'; // output "Ζ"
+        char kappaSymbol2 = '\u039A'; // output "Κ"
+        char upsilonSymbol2 = '\u03A5'; // output "Υ"
+        char psiSymbol2 = '\u03A8'; // output "Ψ"
+        char chiSymbol2 = '\u03A7'; // output "Χ"
+        char betaSymbol2 = '\u0392'; // output "Β"
+        char alphaSymbol2 = '\u0391'; // output "Α"
+
+        // arrays
+        int[] anArray = { 100, 200, 300, 400, 500 };
+
+    }
+}
